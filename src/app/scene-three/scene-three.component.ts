4efc56d0043ced0d9fce39f0d39750a53b8146e2
Zhou Si Cheng  (NCS) <sicheng.zhou@ncs.com.sg>
2019-12-03 12:48:09 +0800

    add album cover

--- src/app/scene-three/scene-three.component.ts
+++ src/app/scene-three/scene-three.component.ts
@@ -10,10 +10,6 @@ export class SceneThreeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // INSERT JS HERE
-
-
-    // SOCIAL PANEL JS
     const floating_btn = document.querySelector('.floating-btn');
     const close_btn = document.querySelector('.close-btn');
     const social_panel_container = document.querySelector('.social-panel-container');
