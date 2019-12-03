0118e2e6257cc2638c4a7647dbe778e176a54fb6
Zhou Si Cheng  (NCS) <sicheng.zhou@ncs.com.sg>
2019-12-03 16:35:33 +0800

    remove unused code

--- src/app/scene-three/scene-three.component.ts
+++ src/app/scene-three/scene-three.component.ts
@@ -10,17 +10,7 @@ export class SceneThreeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const floating_btn = document.querySelector('.floating-btn');
-    const close_btn = document.querySelector('.close-btn');
-    const social_panel_container = document.querySelector('.social-panel-container');
-
-    floating_btn.addEventListener('click', () => {
-      social_panel_container.classList.toggle('visible')
-    });
-
-    close_btn.addEventListener('click', () => {
-      social_panel_container.classList.remove('visible')
-    });
+    
   }
 
 
